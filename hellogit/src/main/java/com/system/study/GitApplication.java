f79ae1aefe7b89ff3e55ae3138214dde1a8438e1
xiaoheStu <1367505472@qq.com>
2019-10-20 19:35:57 +0800

    add first files to git

---
+++ hellogit/src/main/java/com/system/study/GitApplication.java
@@ -0,0 +1,22 @@
+package com.system.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName: GitApplication
+ * Package:com.system.study
+ * Description:
+ *
+ * @Date:2019/10/20 19:28
+ * @Author: hyd132@136.com
+ */
+@SpringBootApplication
+public class GitApplication {
+
+    public static void main(String[] args) {
+        //Git test
+        SpringApplication.run(GitApplication.class, args);
+    }
+
+}
